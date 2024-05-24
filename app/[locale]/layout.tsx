0135bd3b27b99e6a5594c82039fada9f7dbdade8
chrismitsdev <chrismits88@gmail.com>
2024-05-24 18:07:16 +0300

    enabled vercel plugins

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,8 +3,8 @@ import type {Metadata} from 'next'
 import {Arima} from 'next/font/google'
 import {Toaster} from 'react-hot-toast'
 import {Footer} from '@/components/Footer'
-// import {Analytics} from '@vercel/analytics/react'
-// import {SpeedInsights} from '@vercel/speed-insights/next'
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 
 const arima = Arima({
   subsets: ['latin'],
@@ -37,8 +37,8 @@ export default function RootLayout({children, params: {locale}}: {
         {children}
         <Footer locale={locale} />
         <Toaster position='top-right' />
-        {/* <Analytics /> */}
-        {/* <SpeedInsights /> */}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
