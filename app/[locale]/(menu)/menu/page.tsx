f7d1c24c98a2c08ccb1299dd0f7bfb566cf5253b
chrismitsdev <chrismits88@gmail.com>
2024-11-21 21:19:56 +0200

    explicit perma redirect menu page

--- app/[locale]/(menu)/menu/page.tsx
+++ app/[locale]/(menu)/menu/page.tsx
@@ -1,11 +1,6 @@
 import {permanentRedirect} from 'next/navigation'
-import * as React from 'react'
 import {getTranslations} from 'next-intl/server'
 import {getServicesPromise} from '@/lib/promises/getServicesPromise'
-import {getAllCategoriesPromise} from '@/lib/promises/getAllCategoriesPromise'
-import {Container} from '@/components/Container'
-import {Section} from '@/components/Section'
-import {AllCategories} from '@/components/page/menu/AllCategories'
 
 export async function generateMetadata({params: {locale}}: Params) {
   const t = await getTranslations({locale, namespace: 'Metadata.Pages'})
@@ -20,16 +15,40 @@ export async function generateMetadata({params: {locale}}: Params) {
   }
 }
 
-export default async function MenuPage({params: {locale}}: Params) {
+export default function MenuPage() {
   permanentRedirect('https://thechristmaslighthouse.gr')
-
-  // const {categories} = await getAllCategoriesPromise(locale)
-
-  // return (
-  //   <Container>
-  //     <Section className='space-y-6'>
-  //       <AllCategories categories={categories} />
-  //     </Section>
-  //   </Container>
-  // )
 }
+
+// import {permanentRedirect} from 'next/navigation'
+// import * as React from 'react'
+// import {getTranslations} from 'next-intl/server'
+// import {getServicesPromise} from '@/lib/promises/getServicesPromise'
+// import {getAllCategoriesPromise} from '@/lib/promises/getAllCategoriesPromise'
+// import {Container} from '@/components/Container'
+// import {Section} from '@/components/Section'
+// import {AllCategories} from '@/components/page/menu/AllCategories'
+
+// export async function generateMetadata({params: {locale}}: Params) {
+//   const t = await getTranslations({locale, namespace: 'Metadata.Pages'})
+//   const {cafeSection} = await getServicesPromise(locale)
+
+//   return {
+//     title: `${t('Menu')} | Yuppii Luna Park`,
+//     openGraph: {
+//       title: `Yuppii ${t('Menu')}`,
+//       description: cafeSection.subtitle
+//     }
+//   }
+// }
+
+// export default async function MenuPage({params: {locale}}: Params) {
+//   const {categories} = await getAllCategoriesPromise(locale)
+
+//   return (
+//     <Container>
+//       <Section className='space-y-6'>
+//         <AllCategories categories={categories} />
+//       </Section>
+//     </Container>
+//   )
+// }
