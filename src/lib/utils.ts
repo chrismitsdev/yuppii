d28a6873c80ff9c66d8cc63adbb25a7cd8461198
chrismitsdev <chrismits88@gmail.com>
2026-03-16 10:42:43 +0200

    chore: remove unused helper fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,15 +21,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// export function formatCurrency(price: number | string) {
-//   const amount = typeof price === 'string' ? Number.parseFloat(price) : price
-
-//   return new Intl.NumberFormat('el-GR', {
-//     style: 'currency',
-//     currency: 'EUR'
-//   }).format(amount)
-// }
-
 export async function sleep(sleepTime: number = 1000) {
   await new Promise((resolve) => setTimeout(resolve, sleepTime))
 }
